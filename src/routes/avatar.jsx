d03002fddffcc9036f18fa45c03d1ca4cfef80a7
tgraha09 <tfire09@gmail.com>
2022-02-07 12:59:00 -0500

    Cloudflare

--- src/routes/avatar.jsx
+++ src/routes/avatar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import ReactDOM, { render } from 'react-dom'
 import { BrowserRouter as Router, Route, Switch, Link, Redirect, BrowserRouter } from 'react'
 import './styles/avatar.css'
+import avi from './profile.jpg';
 //profile.jpg
 export const Avatar = ()=>{
 
     return(<div className="avatar">
-    <img  src="profile.jpg"  id="profile" alt="avatar"></img>
+    <img  src={avi}  id="profile" alt="avatar"></img>
     <h2 className="taurian"></h2>
   </div>)
 }
