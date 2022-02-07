fa2ac58fe353eef8776e5f5fd778bed46fae1991
tgraha09 <tfire09@gmail.com>
2022-02-07 14:13:57 -0500

    Fix

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -18,7 +18,7 @@ const aboutContent = ()=>{
             </p>
 
             <div className="contact-wrap">
-                <a className="tkg-button" id="link-resume" href="../src/media/resume.pdf"><h2>Resume</h2></a>
+                <a className="tkg-button" id="link-resume" href="resume.pdf"><h2>Resume</h2></a>
                 <Link className="tkg-button" id="link-email" to="/about/email"><h2>Email</h2></Link>
         </div>
     </>)
