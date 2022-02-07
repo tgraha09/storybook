01c81a5a4386843614cce6c06263dcac92892574
tgraha09 <tfire09@gmail.com>
2022-02-07 12:33:35 -0500

    Cloudflare

--- src/routes/avatar.jsx
+++ src/routes/avatar.jsx
@@ -6,7 +6,7 @@ import './styles/avatar.css'
 export const Avatar = ()=>{
 
     return(<div className="avatar">
-    <img  src="../src/media/pic.png"  id="profile" alt="avatar"></img>
+    <img  src="./src/media/pic.png"  id="profile" alt="avatar"></img>
     <h2 className="taurian"></h2>
   </div>)
 }
